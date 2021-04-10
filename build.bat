372a848aa89e375e13250dbcc5fa389d400ccb7b
Krzem <krzem5.dev@gmail.com>
2021-04-10 09:31:32 +0200

    Push Update 04/10/2021, 09:31:12

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/hilbert_curver/Main.java&&jar cvmf ../manifest.mf ../build/hilbert_curver.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="hilbert_curver.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/hilbert_curver.jar
-:end
+python build.py %*
